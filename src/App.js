53357fd94f95caa16eb60996740613898e1a60fa
cono07 <amy_oconnor07@hotmail.co.uk>
2022-02-24 14:14:59 +0000

    Initial file structure set up. Started to create compoent for UserIngredients.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <UserIngredients></UserIngredients>
     </div>
   );
 }
 
 export default App;
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Recipe App</h1>
+      <p>
+        Welcome to our recipe app. Please add some ingredients to find some
+        recipes!
+      </p>
+    </header>
+  );
+};
+
+const UserIngredients = () => {
+  return (
+    <form>
+      <label>Please type an ingredient here to add it to your list: </label>
+      <input type="text"></input>
+      <button type="submit">Submit</button>
+      {/* FIND RECIPES BUTTON NEEDED */}
+    </form>
+  );
+};
