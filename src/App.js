545ca8c1cf6d3b53eb4deb8f11d18a2c837986b0
“mshabuo” <moe.shabuo@hotmail.co.uk>
2022-02-24 16:00:20 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
 
 function App() {
+
+  const recipeListArr = [{
+  recipeName: 'Veggie Pizza', 
+  ingredient: ['tomato', 'flour', 'cheese', 'peppers', 'mushrooms'],
+}, 
+{
+   recipeName: 'Meaty Pizza', 
+  ingredient: ['tomato', 'flour', 'cheese', 'ham', 'chicken', 'beef'],
+},
+{
+   recipeName: 'Margherita Pizza', 
+  ingredient: ['tomato', 'flour', 'cheese'],
+},
+{
+   recipeName: 'Sea Pizza', 
+  ingredient: ['tomato', 'flour', 'cheese', 'prawns'],
+}]
+
+const allIngredients = recipeListArr.map((recipe) => {
+  return recipe
+})
+
+const ingredient = allIngredients['ingredient'].map((ingredient) => {
+  return ingredient
+})
+
+console.log(ingredient)
   return (
     <div className="App">
       <Header></Header>
       <UserIngredients></UserIngredients>
+      <RecipeList recipeListArr={recipeListArr}></RecipeList>
     </div>
   );
 }
@@ -28,8 +56,32 @@ const UserIngredients = () => {
     <form>
       <label>Please type an ingredient here to add it to your list: </label>
       <input type="text"></input>
-      <button type="submit">Submit</button>
-      {/* FIND RECIPES BUTTON NEEDED */}
+      <button type="submit" id="submitButton">Submit</button>
+      <button type="submit" id="findRecipe">Find Recipe</button>
     </form>
   );
+};
+
+const RecipeList = ({recipeListArr}) => {
+
+ return (
+   <main>
+     {recipeListArr.map((recipe)=>{
+       return <div className="recipeItemContainer">
+         <h2 key={recipe.recipeName} className="recipeTitle">
+           {recipe.recipeName}
+         </h2>
+         <p className="ingredientsTitle">Ingredients:</p>
+         <ul>
+           {recipe['ingredient'].map((ingredient)=>{
+             return <li className="ingredientItem" key={recipe.recipeName + ingredient}>
+               {ingredient}
+             </li>
+           })}
+         </ul>
+       </div>
+     })
+     }
+   </main>
+ ) 
 };
