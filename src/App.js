5952eb9634f67749c893068ef22782719ca05ee3
cono07 <amy_oconnor07@hotmail.co.uk>
2022-02-24 18:33:53 +0000

    Created dropdown list of unique ingredients.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import "./App.css";
 
 function App() {
+  const recipeListArr = [
+    {
+      recipeName: "Veggie Pizza",
+      ingredient: ["tomato", "flour", "cheese", "peppers", "mushrooms"],
+    },
+    {
+      recipeName: "Meaty Pizza",
+      ingredient: ["tomato", "flour", "cheese", "ham", "chicken", "beef"],
+    },
+    {
+      recipeName: "Margherita Pizza",
+      ingredient: ["tomato", "flour", "cheese"],
+    },
+    {
+      recipeName: "Sea Pizza",
+      ingredient: ["tomato", "flour", "cheese", "prawns"],
+    },
+  ];
 
-  const recipeListArr = [{
-  recipeName: 'Veggie Pizza', 
-  ingredient: ['tomato', 'flour', 'cheese', 'peppers', 'mushrooms'],
-}, 
-{
-   recipeName: 'Meaty Pizza', 
-  ingredient: ['tomato', 'flour', 'cheese', 'ham', 'chicken', 'beef'],
-},
-{
-   recipeName: 'Margherita Pizza', 
-  ingredient: ['tomato', 'flour', 'cheese'],
-},
-{
-   recipeName: 'Sea Pizza', 
-  ingredient: ['tomato', 'flour', 'cheese', 'prawns'],
-}]
+  const allRecipes = recipeListArr
+    .map((recipe) => {
+      return recipe.ingredient;
+    })
+    .map((ingredient) => {
+      return ingredient;
+    })
+    .flat();
 
-const allIngredients = recipeListArr.map((recipe) => {
-  return recipe
-})
+  const uniqueIngredients = [...new Set(allRecipes)];
 
-const ingredient = allIngredients['ingredient'].map((ingredient) => {
-  return ingredient
-})
-
-console.log(ingredient)
   return (
     <div className="App">
       <Header></Header>
-      <UserIngredients></UserIngredients>
+      <UserIngredients ingredients={uniqueIngredients}></UserIngredients>
       <RecipeList recipeListArr={recipeListArr}></RecipeList>
     </div>
   );
@@ -51,37 +54,52 @@ const Header = () => {
   );
 };
 
-const UserIngredients = () => {
+const DropdownIngredients = ({ ingredients }) => {
+  console.log("in comp: ", ingredients);
+  return (
+    <select name="ingredients" id="ingredients">
+      {ingredients.map((ingredient) => {
+        return <option key={ingredient}>{ingredient}</option>;
+      })}
+    </select>
+  );
+};
+
+const UserIngredients = ({ ingredients }) => {
   return (
     <form>
       <label>Please type an ingredient here to add it to your list: </label>
-      <input type="text"></input>
-      <button type="submit" id="submitButton">Submit</button>
-      <button type="submit" id="findRecipe">Find Recipe</button>
+      <DropdownIngredients ingredients={ingredients}></DropdownIngredients>
+      <button type="submit" id="submitButton">
+        Add Ingredient
+      </button>
+      <button type="submit" id="findRecipe">
+        Find Recipe
+      </button>
     </form>
   );
 };
 
-const RecipeList = ({recipeListArr}) => {
-
- return (
-   <main>
-     {recipeListArr.map((recipe)=>{
-       return <div className="recipeItemContainer">
-         <h2 key={recipe.recipeName} className="recipeTitle">
-           {recipe.recipeName}
-         </h2>
-         <p className="ingredientsTitle">Ingredients:</p>
-         <ul>
-           {recipe['ingredient'].map((ingredient)=>{
-             return <li className="ingredientItem" key={recipe.recipeName + ingredient}>
-               {ingredient}
-             </li>
-           })}
-         </ul>
-       </div>
-     })
-     }
-   </main>
- ) 
+const RecipeList = ({ recipeListArr }) => {
+  return (
+    <main>
+      {recipeListArr.map((recipe) => {
+        return (
+          <div className="recipeItemContainer" key={recipe.recipeName}>
+            <h2 className="recipeTitle">{recipe.recipeName}</h2>
+            <p className="ingredientsTitle">Ingredients:</p>
+            <ul className="ingredientsList">
+              {recipe["ingredient"].map((ingredient) => {
+                return (
+                  <li className="ingredientItem" key={ingredient}>
+                    {ingredient}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </main>
+  );
 };
